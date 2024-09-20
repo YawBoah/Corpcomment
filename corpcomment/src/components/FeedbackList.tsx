241ebd4f2ae8290581d00e4f381224dfa159f6ea
YBoah <yawboahene.yb@gmail.com>
2024-09-20 14:41:41 +0000

    The loading effect while the data is been fetched

--- corpcomment/src/components/FeedbackList.tsx
+++ corpcomment/src/components/FeedbackList.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
 
 
 const FeedbackList = () => {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks")
       .then((response) => response.json())
-      .then((data) => setFeedbackItems(data.feedbacks));
+      .then((data) => {setFeedbackItems(data.feedbacks);
+      setIsLoading(false);
+  });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem feedbackItem={feedbackItem} key={feedbackItem.id}/>
       ))}
