aa82df227bfdb53819822a453dbf1249dd87adf1
YBoah <yawboahene.yb@gmail.com>
2024-09-30 14:13:33 +0000

    Disable Upvote Button

--- corpcomment/src/components/feedback/FeedbackItem.tsx
+++ corpcomment/src/components/feedback/FeedbackItem.tsx
@@ -11,10 +11,16 @@ const FeedbackItem = ({ feedbackItem }: FeedbackItemProps) => {
   const [open, setOpen] = useState(false);
   const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
 
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount((prev) => ++prev);
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
+
   return (
     <li onClick={() => setOpen((prev) => !prev)} className={`feedback ${open ? 'feedback--expand': ''}`}>
 
-      <button onClick={() => setUpvoteCount((prev)=> ++prev)}>
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
         <span>{upvoteCount}</span>
       </button>
