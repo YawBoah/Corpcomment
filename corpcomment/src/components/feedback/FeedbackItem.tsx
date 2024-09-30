bfea2a5427196a853f146b9bc5ee28124569a395
YBoah <yawboahene.yb@gmail.com>
2024-09-30 14:00:10 +0000

    Upvote feature

--- corpcomment/src/components/feedback/FeedbackItem.tsx
+++ corpcomment/src/components/feedback/FeedbackItem.tsx
@@ -9,12 +9,14 @@ type FeedbackItemProps = {
 const FeedbackItem = ({ feedbackItem }: FeedbackItemProps) => {
 
   const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
 
   return (
     <li onClick={() => setOpen((prev) => !prev)} className={`feedback ${open ? 'feedback--expand': ''}`}>
-      <button>
+
+      <button onClick={() => setUpvoteCount((prev)=> ++prev)}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
 
       <div>
